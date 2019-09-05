2fac0c6608273a19338a8f7454e8898749a21bb1
sboonchusanong <litty_135@hotmail.com>
2019-09-05 22:49:50 +0700

    edit MovieRepository.java / MoviesController.java
    pass test MoviesControllerIntegrationTest
    
    แก้ไข bean `simpleMovieSearchService` เป็น `databaseMovieSearchService` ในไฟล์ `MoviesController`

--- src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
+++ src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
@@ -2,6 +2,9 @@ package com.wongnai.interview.movie.sync;
 
 import javax.transaction.Transactional;
 
+import com.wongnai.interview.movie.Movie;
+import com.wongnai.interview.movie.external.MovieData;
+import com.wongnai.interview.movie.external.MoviesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,5 +22,11 @@ public class MovieDataSynchronizer {
 	@Transactional
 	public void forceSync() {
 		//TODO: implement this to sync movie into repository
+		MoviesResponse allData=movieDataService.fetchAll();
+		for(MovieData i:allData){
+			Movie movie=new Movie(i.getTitle());
+			movie.setActors(i.getCast());
+			movieRepository.save(movie);
+		}
 	}
 }
