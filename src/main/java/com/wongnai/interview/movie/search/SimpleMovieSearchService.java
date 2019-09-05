55257f31cc0379cceb93bcdc6d0d288b9a9931aa
sboonchusanong <litty_135@hotmail.com>
2019-09-05 21:12:22 +0700

    edit SimpleMovieSearchService.java
    pass test `SimpleMovieSearchServiceIntegrationTest` and `MoviesControllerIntegrationTest`

--- src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
@@ -1,7 +1,11 @@
 package com.wongnai.interview.movie.search;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import com.wongnai.interview.movie.external.MovieData;
+import com.wongnai.interview.movie.external.MoviesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +23,28 @@ public class SimpleMovieSearchService implements MovieSearchService {
 		//TODO: Step 2 => Implement this method by using data from MovieDataService
 		// All test in SimpleMovieSearchServiceIntegrationTest must pass.
 		// Please do not change @Component annotation on this class
-		return null;
+		List<Movie> result=new ArrayList<>();
+		if(queryText.contains(" ")){
+			//no space allow in query
+			return result;
+		}
+		MoviesResponse data=movieDataService.fetchAll();
+
+		//transform title into list of words separate by space, then check if querytext is in list
+		queryText=queryText.toLowerCase();
+		for (MovieData each : data){
+			List<String> words= Arrays.asList(each.getTitle().split(" "));
+			words.replaceAll(String::toLowerCase);
+			if(words.contains(queryText)){
+				Movie movie=new Movie(each.getTitle());
+				movie.setActors(each.getCast());
+				result.add(movie);
+
+			}
+		}
+
+
+
+		return result;
 	}
 }
